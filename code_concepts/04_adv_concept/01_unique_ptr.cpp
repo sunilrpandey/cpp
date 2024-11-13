acaea1b967bb7364ce2998c3ff45da6cbd462715
sunilrpandey <inkingmind@gmail.com>
2024-11-13 09:55:25 +0530

    more examples added

--- code_concepts/04_adv_concept/01_unique_ptr.cpp
+++ code_concepts/04_adv_concept/01_unique_ptr.cpp
@@ -10,16 +10,16 @@ class Dog
 
     Dog(const string & str = ""):name_(str){
         name_.append(to_string(counter++));
-        cout <<  "\nBirth : " << name_.c_str() << std::endl; 
+        cout <<  "\nBirth : " << name_.c_str() << std::endl;
     }
     Dog(const initializer_list<string>& vec_str){
         name_ = *(vec_str.begin());
         Dog(name_);
         //name_.append(to_string(counter++));
-        //cout << "init_list Birth : " << name_.c_str() << endl; 
+        //cout << "init_list Birth : " << name_.c_str() << endl;
     }
     ~Dog(){
-        cout << "\nDeath : " << name_.c_str() << std::endl; 
+        cout << "\nDeath : " << name_.c_str() << std::endl;
     }
     void bark(){
         cout << "I am " << name_.c_str() << endl;
@@ -31,13 +31,39 @@ unique_ptr<Dog> getPtr(const std::string& dname, bool make_unique_flag = true)
     if(make_unique_flag){
         return make_unique<Dog>(dname);
     }
-    
+
     unique_ptr<Dog> p(new Dog(dname));
     //p->bark();
     return p;
 }
 int main()
 {
+    auto uip = std::unique_ptr<int> (new int);
+    *uip = 12;
+    std::cout << "instantiated using basic method :" << *uip << std::endl;
+
+    auto uip1 = std::make_unique<int>(15);
+    std::cout << "instantiated using make_unique method :" << std::endl;
+
+    std::unique_ptr<Dog> uidg = std::unique_ptr<Dog>(new Dog("Tommy"));
+    uidg->bark();
+
+    auto uidg1 = std::unique_ptr<Dog>(new Dog("Tommy_unique"));
+    uidg1->bark();
+
+    uidg1.get()->bark();
+
+    //assign new pointer using = or reset
+    uidg.reset(uidg1.get());
+
+
+
+
+    return 0;
+
+
+
+
     // you can use new operator or make_unique to initialize
     bool use_make_unique = false;
     unique_ptr<Dog> p = getPtr("Smoky", use_make_unique);
@@ -49,7 +75,7 @@ int main()
     if(!p){
         cout  << endl << "no more p" << endl;
     }
-    
+
     unique_ptr<Dog> emptyPtr = nullptr;
     emptyPtr = move(p2);
     cout << "\nEmptyPtr got something : " << emptyPtr->name_;
@@ -57,15 +83,15 @@ int main()
     //reset deletes the resource underneath
     cout << endl << "See how NewDog replaces old one";
     emptyPtr.reset(new Dog("NewDog"));
-    
-    
+
+
     cout << "\n->->Demo : make unique <-<- ";
     auto dgPtr = make_unique<Dog>("Madebymakeunique");
     cout << "\n Name of this dog: " << dgPtr->name_ << endl;
 
-    auto releasedFromDgPtr = dgPtr.release(); 
+    auto releasedFromDgPtr = dgPtr.release();
     cout << endl << "releasedFromDgPtr : " << releasedFromDgPtr->name_;
-    
+
     delete releasedFromDgPtr;
     return 0;
 }
